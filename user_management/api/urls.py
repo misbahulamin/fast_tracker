fa7a11ff37db1ba957e28f12db7f6c3394e031e2
misbahulamin <misbahul.amin2001@gmail.com>
2025-01-07 20:37:09 +0800

    done

--- user_management/api/urls.py
+++ user_management/api/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeNameAPIView, UserRegistrationView, EmployeeListAPIView, UserLoginApiView, UserLogoutView, UserListView, DepartmentViewSet, DesignationViewSet
+from .views import EmployeeNameAPIView, UserRegistrationView, EmployeeListAPIView, UserLoginApiView, UserLogoutView, UserListView, DepartmentViewSet, DesignationViewSet, AddEmployeeViewset
 
 router = DefaultRouter()
 router.register(r'department', DepartmentViewSet, basename='department')
 router.register(r'designation', DesignationViewSet, basename='designation')
+router.register(r'add-employee', AddEmployeeViewset, basename='add-employee')
 
 urlpatterns = [
     path('', include(router.urls)),  # Include router URLs
     path('register/', UserRegistrationView.as_view(), name='register'),  # Add a non-viewset view
     path('login/', UserLoginApiView.as_view(), name='login'),
     path('employee-list/', EmployeeListAPIView.as_view(), name='employee-list'),
-    path('userlist/', UserListView.as_view(), name='employee-list'),
+    path('user-list/', UserListView.as_view(), name='user-list'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('employee-details/', EmployeeNameAPIView.as_view(), name='employee-details'),
 ]
